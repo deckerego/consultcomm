9966c1cbce20f0e0b055af44ffa39614102edf32
jtellis <jtellis@684d808d-fa0d-0410-8fdc-84466df7d49a>
2001-10-12 19:39:39 +0000

    Nuthin' much
    git-svn-id: svn+ssh://svn.code.sf.net/p/consultcomm/code/trunk@81 684d808d-fa0d-0410-8fdc-84466df7d49a

--- ConsultComm/CsltComm.java
+++ ConsultComm/CsltComm.java
@@ -7,7 +7,7 @@ import java.io.*;
 import ClntComm;
 
 public class CsltComm extends javax.swing.JFrame {
-  public static final String release = "ConsultComm 2.0";
+  public static final String release = "ConsultComm 2.0 Beta";
   final static File prefsDir = new File(System.getProperty("user.home")+System.getProperty("file.separator")+"CsltComm");
   static MediaTracker iconTracker;
   static MTPanel iconPanel;
@@ -40,7 +40,6 @@ public class CsltComm extends javax.swing.JFrame {
     }
     initComponents();
     prefsDir.mkdir();
-    System.out.println("User directory: "+prefsDir);
     
     iconTracker = new MediaTracker(this);
     iconTracker.addImage(clockIcon, 0);
