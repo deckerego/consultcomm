9e4dc461179771e16c1f8e3d94dabca29e4ab213
jtellis <jtellis@684d808d-fa0d-0410-8fdc-84466df7d49a>
2002-12-30 18:34:48 +0000

    Rewritten by Dan Price
    git-svn-id: svn+ssh://svn.code.sf.net/p/consultcomm/code/trunk@330 684d808d-fa0d-0410-8fdc-84466df7d49a

--- TimeOutBean/X/timeout.c
+++ TimeOutBean/X/timeout.c
@@ -2,31 +2,56 @@
 #include <jni.h>
 #include <X11/Xlib.h>
 #include <X11/extensions/scrnsaver.h>
-#include "ClntComm.h"
+#include "TimeOut.h"
 
-unsigned long get_x_idle_time ( )
-{
-  unsigned long idle;
+/**
+ * Major thanks to Dan Price for (re)writing this whole library to
+ * work correctly, even under Solaris and VNC. Thanks also for
+ * plugging the memory leaks and error detection.
+*/
+static int get_x_idle_time(long *idle) {
   XScreenSaverInfo *ss_info = NULL;
   Display *display;
+  static int firsttime = 1; //Is this the first time we've run this method?
 
-  display = XOpenDisplay(NULL);
-  if(display == NULL) 
+  if((display = XOpenDisplay(NULL)) == NULL) {
     printf("Display is null\n");
+    return(-1);
+  }
 
-  ss_info = XScreenSaverAllocInfo ();
-  XScreenSaverQueryInfo ( display, DefaultRootWindow ( display ), ss_info );
-  idle = ss_info->idle;
+  if(firsttime) {
+    int evb, erb;
+    if(XScreenSaverQueryExtension(display, &evb, &erb) == False) {
+      XCloseDisplay(display);
+      return(-2);
+    }
+    firsttime = 0;
+  }
 
+  if((ss_info = XScreenSaverAllocInfo()) == NULL) {
+    XCloseDisplay(display);
+    return(-1);
+  }
+
+  if(XScreenSaverQueryInfo(display, DefaultRootWindow(display), ss_info) == 0) {
+    XFree(ss_info);
+    XCloseDisplay(display);
+    return(-1);
+  }
+  *idle = ss_info->idle;
+
+  XFree(ss_info);
   XCloseDisplay(display);
-
-  return ( idle );
-}
-JNIEXPORT jlong JNICALL Java_ClntComm_getIdleTime (JNIEnv *env, jobject obj)
-{
-  return ( get_x_idle_time() );
+  return(0);
 }
 
-int main ( int argc, char *argv[] ) {
-  fprintf(stdout, "Idle time: %d", get_x_idle_time());
+JNIEXPORT jlong JNICALL Java_ClntComm_getIdleTime(JNIEnv *env, jobject obj) {
+  int ret;
+  long time = 0;
+
+  ret = get_x_idle_time(&time);
+
+  if(ret != 0) return(time);
+  return(0);
 }
+
