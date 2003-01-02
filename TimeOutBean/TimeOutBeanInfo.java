705ba16f35c214233e8b469e18e8066a04830339
jtellis <jtellis@684d808d-fa0d-0410-8fdc-84466df7d49a>
2003-01-02 15:41:55 +0000

    Code cleanup
    git-svn-id: svn+ssh://svn.code.sf.net/p/consultcomm/code/trunk@345 684d808d-fa0d-0410-8fdc-84466df7d49a

--- TimeOutBean/TimeOutBeanInfo.java
+++ TimeOutBean/TimeOutBeanInfo.java
@@ -9,35 +9,29 @@ public class TimeOutBeanInfo extends SimpleBeanInfo {
     private static BeanDescriptor getBdescriptor(){
         //GEN-HEADEREND:BeanDescriptor
         beanDescriptor = new BeanDescriptor(TimeOut.class, TimeOutCustomizer.class);
-        
         return beanDescriptor;     } //GEN-LAST:BeanDescriptor
     
     
     // Properties information will be obtained from introspection.//GEN-FIRST:Properties
     private static PropertyDescriptor[] properties = null;
     private static PropertyDescriptor[] getPdescriptor(){//GEN-HEADEREND:Properties
-
-        return properties;     } //GEN-LAST:Properties
+                return properties;     } //GEN-LAST:Properties
     
     // Event set information will be obtained from introspection.//GEN-FIRST:Events
     private static EventSetDescriptor[] eventSets = null;
     private static EventSetDescriptor[] getEdescriptor(){//GEN-HEADEREND:Events
-
-        return eventSets;     } //GEN-LAST:Events
+                return eventSets;     } //GEN-LAST:Events
     
     // Method information will be obtained from introspection.//GEN-FIRST:Methods
     private static MethodDescriptor[] methods = null;
     private static MethodDescriptor[] getMdescriptor(){//GEN-HEADEREND:Methods
-        
-        // Here you can add code for customizing the methods array.
-        
-        return methods;     } //GEN-LAST:Methods
+                return methods;     } //GEN-LAST:Methods
     
     private static java.awt.Image iconColor16 = null; //GEN-BEGIN:IconsDef
     private static java.awt.Image iconColor32 = null;
     private static java.awt.Image iconMono16 = null;
     private static java.awt.Image iconMono32 = null; //GEN-END:IconsDef
-    private static String iconNameC16 = null; //GEN-BEGIN:Icons
+        private static String iconNameC16 = null; //GEN-BEGIN:Icons
     private static String iconNameC32 = null;
     private static String iconNameM16 = null;
     private static String iconNameM32 = null; //GEN-END:Icons
@@ -47,8 +41,6 @@ public class TimeOutBeanInfo extends SimpleBeanInfo {
     
     
     //GEN-FIRST:Superclass
-    
-    // Here you can add code for customizing the Superclass BeanInfo.
     
     //GEN-LAST:Superclass
     
@@ -61,67 +53,6 @@ public class TimeOutBeanInfo extends SimpleBeanInfo {
      */
     public BeanDescriptor getBeanDescriptor() {
         return getBdescriptor();
-    }
-    
-    /**
-     * Gets the bean's <code>PropertyDescriptor</code>s.
-     *
-     * @return An array of PropertyDescriptors describing the editable
-     * properties supported by this bean.  May return null if the
-     * information should be obtained by automatic analysis.
-     * <p>
-     * If a property is indexed, then its entry in the result array will
-     * belong to the IndexedPropertyDescriptor subclass of PropertyDescriptor.
-     * A client of getPropertyDescriptors can use "instanceof" to check
-     * if a given PropertyDescriptor is an IndexedPropertyDescriptor.
-     */
-    public PropertyDescriptor[] getPropertyDescriptors() {
-        return getPdescriptor();
-    }
-    
-    /**
-     * Gets the bean's <code>EventSetDescriptor</code>s.
-     *
-     * @return  An array of EventSetDescriptors describing the kinds of
-     * events fired by this bean.  May return null if the information
-     * should be obtained by automatic analysis.
-     */
-    public EventSetDescriptor[] getEventSetDescriptors() {
-        return getEdescriptor();
-    }
-    
-    /**
-     * Gets the bean's <code>MethodDescriptor</code>s.
-     *
-     * @return  An array of MethodDescriptors describing the methods
-     * implemented by this bean.  May return null if the information
-     * should be obtained by automatic analysis.
-     */
-    public MethodDescriptor[] getMethodDescriptors() {
-        return getMdescriptor();
-    }
-    
-    /**
-     * A bean may have a "default" property that is the property that will
-     * mostly commonly be initially chosen for update by human's who are
-     * customizing the bean.
-     * @return  Index of default property in the PropertyDescriptor array
-     * 		returned by getPropertyDescriptors.
-     * <P>	Returns -1 if there is no default property.
-     */
-    public int getDefaultPropertyIndex() {
-        return defaultPropertyIndex;
-    }
-    
-    /**
-     * A bean may have a "default" event that is the event that will
-     * mostly commonly be used by human's when using the bean.
-     * @return Index of default event in the EventSetDescriptor array
-     *		returned by getEventSetDescriptors.
-     * <P>	Returns -1 if there is no default event.
-     */
-    public int getDefaultEventIndex() {
-        return defaultEventIndex;
     }
     
     /**
@@ -147,6 +78,7 @@ public class TimeOutBeanInfo extends SimpleBeanInfo {
      */
     public java.awt.Image getIcon(int iconKind) {
         iconNameC32 = "timer.gif";
+        
         switch ( iconKind ) {
             case ICON_COLOR_16x16:
                 if ( iconNameC16 == null )
@@ -183,6 +115,4 @@ public class TimeOutBeanInfo extends SimpleBeanInfo {
             default: return null;
         }
     }
-    
 }
-
