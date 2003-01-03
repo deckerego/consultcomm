577356c2c153337c1c3d45a7831a6905df4ec0e2
jtellis <jtellis@684d808d-fa0d-0410-8fdc-84466df7d49a>
2003-01-03 16:06:54 +0000

    Reconciled with patch
    git-svn-id: svn+ssh://svn.code.sf.net/p/consultcomm/code/trunk@359 684d808d-fa0d-0410-8fdc-84466df7d49a

--- TimeOutBean/X/timeout.c
+++ TimeOutBean/X/timeout.c
@@ -5,8 +5,8 @@
 #include "TimeOut.h"
 
 /**
- * Major thanks to Dan Price for (re)writing this whole library to
- * work correctly, even under Solaris and VNC. Thanks also for
+ * Major thanks to Dan Price for helping to rewrite this library 
+ * to work correctly under Solaris and VNC. Thanks also for
  * plugging the memory leaks and error detection.
 */
 static int get_x_idle_time(long *idle) {
@@ -15,13 +15,14 @@ static int get_x_idle_time(long *idle) {
   static int firsttime = 1; //Is this the first time we've run this method?
 
   if((display = XOpenDisplay(NULL)) == NULL) {
-    printf("Display is null\n");
+    printf("Couldn't open display\n");
     return(-1);
   }
 
   if(firsttime) {
     int evb, erb;
     if(XScreenSaverQueryExtension(display, &evb, &erb) == False) {
+      printf("No screen saver extension\n");
       XCloseDisplay(display);
       return(-2);
     }
@@ -29,11 +30,13 @@ static int get_x_idle_time(long *idle) {
   }
 
   if((ss_info = XScreenSaverAllocInfo()) == NULL) {
+    printf("Could not alloc screen saver info\n");
     XCloseDisplay(display);
     return(-1);
   }
 
   if(XScreenSaverQueryInfo(display, DefaultRootWindow(display), ss_info) == 0) {
+    printf("Could not query root window\n");
     XFree(ss_info);
     XCloseDisplay(display);
     return(-1);
@@ -45,13 +48,13 @@ static int get_x_idle_time(long *idle) {
   return(0);
 }
 
-JNIEXPORT jlong JNICALL Java_ClntComm_getIdleTime(JNIEnv *env, jobject obj) {
+JNIEXPORT jlong JNICALL Java_TimeOut_getIdleTime(JNIEnv *env, jobject obj) {
   int ret;
   long time = 0;
 
   ret = get_x_idle_time(&time);
 
-  if(ret != 0) return(time);
-  return(0);
+  if(ret == 0) return(time);
+  return(ret);
 }
 
