6a9b1838ddf8169703beed73476adaa577235053
jtellis <jtellis@684d808d-fa0d-0410-8fdc-84466df7d49a>
2004-08-25 02:31:03 +0000

    Win32 fix
    git-svn-id: svn+ssh://svn.code.sf.net/p/consultcomm/code/trunk@526 684d808d-fa0d-0410-8fdc-84466df7d49a

--- SysTrayBean/SysTray.java
+++ SysTrayBean/SysTray.java
@@ -6,48 +6,62 @@ import java.awt.event.*;
 
 
 public class SysTray extends CsltCommPlugin implements ActionListener, ItemListener {
+  private static final long CLICK_THRESHHOLD = 500;
+
   private static boolean systrayLibrary = false;
   private TrayIcon sysTrayIcon;
   private SystemTray tray;
   private ClntComm clntComm;
-  
+  private long lastClicked;
+      
   static {
-    if(JarLoader.loadNativeLibrary("libtray.so", SysTray.class) && JarLoader.loadNativeLibrary("tray.dll", SysTray.class)) {
-      systrayLibrary = true;
-    } else {
-      System.err.println("Couldn't find systray library in "+System.getProperty("java.library.path"));
-      systrayLibrary = false;
-    }
+    //Even on non-*nix platforms we load this file. Doesn't hurt, and ensures that we have a
+    //valid system library with sufficient room and permissions.
+    systrayLibrary = JarLoader.loadNativeLibrary("libtray.so", SysTray.class) == JarLoader.FILE_OK;
+    //Windows .dlls aren't automatically unloaded by Java, both because the JVM remains
+    //resident and because .dlls (even when forced) remain locked and marked as in use.
+    //Therefore it's not a huge surprise that an existing file is still sticking around.
+    //Don't panic if it does; just panic if there was an I/O or other error thrown.
+    systrayLibrary &= JarLoader.loadNativeLibrary("tray.dll", SysTray.class) != JarLoader.FILE_ERROR;
+    //We don't call the native library here - the Tray API does that for us in the
+    //prepackaged jar we get from JDIC.
   }
   
   public SysTray() {
-    try {
-      JPopupMenu menu;
-      JMenuItem menuItem;
-      
-      tray = SystemTray.getDefaultSystemTray();
-      ImageIcon icon = JarLoader.loadImageIcon("systray_22.png", SysTray.class);
-      sysTrayIcon = new TrayIcon(icon, "Consultant Communicator 3", new JPopupMenu());
-      sysTrayIcon.setIconAutoSize(true);
-      tray.addTrayIcon(sysTrayIcon);
-      
-      sysTrayIcon.addActionListener(new ActionListener() {
-        public void actionPerformed(ActionEvent e) {
-          Component csltCommComponent = clntComm.getTopLevelAncestor();
-          csltCommComponent.setVisible(! csltCommComponent.isVisible());
-        }
-      });
-      
-      menu = new JPopupMenu("A Menu");
-      menuItem = new JMenuItem("Quit", KeyEvent.VK_Q);
-      menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, ActionEvent.ALT_MASK));
-      menuItem.getAccessibleContext().setAccessibleDescription("Quit ConsultComm");
-      menuItem.addActionListener(this);
-      menu.add(menuItem);
-      
-      sysTrayIcon.setPopupMenu(menu);
-    } catch (java.lang.UnsatisfiedLinkError e) {
-      systrayLibrary = false;
+    if(systrayLibrary) {
+      try {
+        JPopupMenu menu;
+        JMenuItem menuItem;
+        
+        tray = SystemTray.getDefaultSystemTray();
+        ImageIcon icon = JarLoader.loadImageIcon("systray_22.png", SysTray.class);
+        sysTrayIcon = new TrayIcon(icon, "Consultant Communicator 3", new JPopupMenu());
+        sysTrayIcon.setIconAutoSize(true);
+        tray.addTrayIcon(sysTrayIcon);
+        
+        menu = new JPopupMenu("A Menu");
+        menuItem = new JMenuItem("Quit", KeyEvent.VK_Q);
+        menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, ActionEvent.ALT_MASK));
+        menuItem.getAccessibleContext().setAccessibleDescription("Quit ConsultComm");
+        menuItem.addActionListener(this);
+        menu.add(menuItem);
+        
+        sysTrayIcon.addActionListener(new ActionListener() {
+          public void actionPerformed(ActionEvent evt) {
+            //The below line is a hack around the Win32 implementation always generating an 
+            //event on every mousedown action, so several of them get submitted with each 
+            //mouse click. Only take one event every CLICK_THRESHHOLD seconds
+            if(evt.getWhen() - lastClicked < CLICK_THRESHHOLD) return;
+            lastClicked = evt.getWhen();
+            Component csltCommComponent = clntComm.getTopLevelAncestor();
+            csltCommComponent.setVisible(! csltCommComponent.isVisible());
+          }
+        });
+        
+        sysTrayIcon.setPopupMenu(menu);
+      } catch (java.lang.UnsatisfiedLinkError e) {
+        systrayLibrary = false;
+      }
     }
   }
   
