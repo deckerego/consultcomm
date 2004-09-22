4112f4913cce8bf5e95b44bd55de87ae33a2a660
jtellis <jtellis@684d808d-fa0d-0410-8fdc-84466df7d49a>
2004-09-22 03:45:28 +0000

    Initial Import
    git-svn-id: svn+ssh://svn.code.sf.net/p/consultcomm/code/trunk@545 684d808d-fa0d-0410-8fdc-84466df7d49a

---
+++ EmptyBean/Empty.java
@@ -0,0 +1,46 @@
+/*
+ * Empty.java
+ *
+ * Created on September 17, 2004, 1:57 PM
+ */
+
+/**
+ * @author http://consultcomm.sourceforge.net
+ */
+public class Empty extends CsltCommPlugin {
+  private ClntComm clntComm;
+  
+  public Empty() {
+  }
+  
+  /**
+   * Called whenever something happens in the main ConsultComm window
+   * (i.e. the timer has incremented or a record has been changed
+   * @param evt The property change event that notifies us of the change
+   */
+  public void propertyChange(java.beans.PropertyChangeEvent evt) {
+    //If we haven't already retreived a reference to ConsultComm, do so now
+    if(clntComm == null) clntComm = (ClntComm)evt.getSource();
+
+    // User has edited a project (changed a name or time amount)
+    if(evt.getPropertyName().equals("record")) {
+      System.out.println("Edited a record");
+    }
+
+    // Timelist has been changed, find out how
+    if(evt.getPropertyName().equals("times")) {
+      //List of times after the change occured
+      TimeRecordSet newTimes = (TimeRecordSet)evt.getNewValue();
+      //List of times before the change occured
+      TimeRecordSet oldTimes = (TimeRecordSet)evt.getOldValue();
+      
+       //User has added a project
+      if(newTimes != null && oldTimes != null && newTimes.size() > oldTimes.size()) {
+        System.out.println("Added a record");
+      } else { //Otherwise, the clock has just incremented
+        System.out.println("Time has changed");
+      }
+    }
+  }
+  
+}
