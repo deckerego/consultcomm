91762e324f88171a8a421034716cb244cdf1ba5c
jtellis <jtellis@684d808d-fa0d-0410-8fdc-84466df7d49a>
2005-02-16 01:59:34 +0000

    Deregister is now handled by window event handler; popup menu can be added to
    git-svn-id: svn+ssh://svn.code.sf.net/p/consultcomm/code/trunk@595 684d808d-fa0d-0410-8fdc-84466df7d49a

--- ConsultComm/src/CsltCommPlugin.java
+++ ConsultComm/src/CsltCommPlugin.java
@@ -5,12 +5,14 @@
  */
 
 /**
- *
- * @author  jellis
+ * Interface for building ConsultComm plugins. Plugins may or may not
+ * offer methods to add to the tools menu or to the main project list
+ * pop-up menu. They must look for property changes and be serializeable
+ * (it's how preferences are usually stored and the plugin's state is retained)
  */
 public abstract class CsltCommPlugin extends Object implements java.io.Serializable, java.lang.Cloneable, java.beans.PropertyChangeListener {
     public abstract void propertyChange(java.beans.PropertyChangeEvent evt);
     
     public javax.swing.JMenuItem[] getMenuItems() { return null; }
-    public void unregister() { }
+    public javax.swing.JMenuItem[] getPopupMenuItems() { return null; }
 }
