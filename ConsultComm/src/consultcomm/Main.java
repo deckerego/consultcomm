701c9bb75e91a6d0ba2d0a7b241532cedfd55bda
jtellis <jtellis@684d808d-fa0d-0410-8fdc-84466df7d49a>
2006-10-02 02:52:09 +0000

    git-svn-id: svn+ssh://svn.code.sf.net/p/consultcomm/code/trunk@628 684d808d-fa0d-0410-8fdc-84466df7d49a

---
+++ ConsultComm/src/consultcomm/Main.java
@@ -0,0 +1,29 @@
+/*
+ * Main.java
+ *
+ * Created on October 1, 2006, 10:44 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package consultcomm;
+
+/**
+ *
+ * @author jellis
+ */
+public class Main {
+    
+    /** Creates a new instance of Main */
+    public Main() {
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
