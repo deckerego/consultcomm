1500d3efd8f0b81b6186324052110a3a1d31f259
jtellis <jtellis@684d808d-fa0d-0410-8fdc-84466df7d49a>
2007-04-01 15:02:32 +0000

    Vastly simplified the editor by using a date formatter. Note this only allows HH:mm:ss, so it's not as flexible.
    git-svn-id: svn+ssh://svn.code.sf.net/p/consultcomm/code/trunk@657 684d808d-fa0d-0410-8fdc-84466df7d49a

--- ConsultComm/src/consultcomm/treetable/TimeEditor.java
+++ ConsultComm/src/consultcomm/treetable/TimeEditor.java
@@ -10,6 +10,8 @@ import java.util.SimpleTimeZone;
 import javax.swing.DefaultCellEditor;
 import javax.swing.JFormattedTextField;
 import javax.swing.JTable;
+import javax.swing.text.DateFormatter;
+import javax.swing.text.DefaultFormatterFactory;
 
 /**
  * Control the components & edited values when editing a table cell.
@@ -20,6 +22,7 @@ public class TimeEditor
     extends DefaultCellEditor
 {
   private DateFormat numberFormat;
+  private DateFormatter numberFormatter;
   
   /** Creates a new instance of TimeEditor */
   public TimeEditor()
@@ -28,22 +31,10 @@ public class TimeEditor
     
     this.numberFormat = new SimpleDateFormat("HH:mm:ss");
     this.numberFormat.setTimeZone(new SimpleTimeZone(0, "NONE"));
-  }
-  
-  /**
-   * Provides a value object that represents the given cell when it is being edited.
-   * @param table The parent component that "owns" the cell
-   * @param value The actual value in the cell
-   * @param isSelected Is the cell currently selected?
-   * @param row The row index of the cell
-   * @param column The column index of the cell
-   * @return The component representing the table's cell
-   */
-  public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column)
-  {
-    JFormattedTextField textField = (JFormattedTextField) super.getTableCellEditorComponent(table, value, isSelected, row, column);
-    textField.setValue(value);
-    return textField;
+    
+    this.numberFormatter = new DateFormatter(numberFormat);
+    
+    ((JFormattedTextField) getComponent()).setFormatterFactory(new DefaultFormatterFactory(this.numberFormatter));
   }
   
   /**
@@ -57,39 +48,8 @@ public class TimeEditor
     JFormattedTextField textField = (JFormattedTextField) getComponent();
     Object textValue = textField.getValue();
     
-    assert textValue instanceof String || textValue instanceof Time;
+    assert textValue instanceof Date;
     
-    if(textValue instanceof String)
-    {
-      String stringValue = (String) textValue;
-      
-      if(stringValue.matches("[0-9]*"))
-      { //Maybe the user just typed in a whole number. Let's assume they meant the number of elapsed minutes
-        Long elapsed = Long.parseLong(stringValue);
-        return new Time(elapsed * 60 * 1000);
-      }
-      
-      else if(stringValue.matches("[0-9]*:[0-9]*"))
-      { //The user may have just typed in hh:mm, so we'll make the string well-formed
-        stringValue += ":00";
-      }
-      
-      try
-      { //Actually parse the date based on the format 24:60:60
-        Date elapsed = this.numberFormat.parse(stringValue);
-        return new Time(elapsed.getTime());
-      }
-      
-      catch(ParseException e)
-      {
-        System.err.println("Couldn't parse "+textValue+": "+e);
-        return textValue;
-      }
-    }
-    
-    else
-    {
-      return textValue;
-    }
+    return new Time(((Date) textValue).getTime());
   }
 }
